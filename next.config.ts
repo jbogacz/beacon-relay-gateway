f50d20ddbbaaa1d711f5dca8f512a72ccd85cace
Jakub Bogacz <bogacz.jakub3@gmail.com>
2025-07-05 10:25:19 +0200

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
@@ -9,6 +10,10 @@ const nextConfig: NextConfig = {
       fs: false,
       path: false,
     };
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, '.'),
+    }
 
     return config;
   },
